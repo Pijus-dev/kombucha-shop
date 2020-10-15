fcf12f46b4093a5dd476166447b91149597fafe3
Pijus <user@users-MBP.lan>
2020-10-15 15:03:32 +0300

    Removed folder from repository

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { persistStore } from "redux-persist";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-import rootReducer from "./rootReducer";
-
-export const store = createStore(rootReducer, composeWithDevTools());
-
-export const persistor = persistStore(store);
